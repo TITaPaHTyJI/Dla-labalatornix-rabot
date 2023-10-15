cff67c6aa286ca0bf16d8719c0393922ff8b6f87
TITaPaHTyJI <146864770+TITaPaHTyJI@users.noreply.github.com>
2023-10-15 15:26:29 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,8 +1,9 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-sum_numbers = sum(numbers[:4]) + sum(numbers[5:])
+missing_index = 4
+sum_numbers = sum(numbers[:missing_index]) + sum(numbers[missing_index + 1:])
 len_numbers = len(numbers)
-average_numbers = sum_numbers/len_numbers
+average_numbers = sum_numbers / len_numbers
 numbers[4] = average_numbers
 
 print("Измененный список:", numbers)
